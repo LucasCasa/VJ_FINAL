f6a55b518e3b9298feb766456be0c01e6b9e5a79
Lucas <lcasagrande@itba.edu.ar>
2017-12-06 18:25:50 -0300

    starting.

---
+++ Assets/LoadBricks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadBricks : MonoBehaviour {
+    public GameObject brickPrefab;
+    public List<ColorToPrefab> bricks;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Pad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.position += new Vector3(horizontal, 0, 0);
+	}
+    void OnCollisionEnter2D(Collision2D coll) {
+        Debug.Log("Hit");
+    }
+}
