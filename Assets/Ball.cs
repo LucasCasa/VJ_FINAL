5c8fb24b797f952251e352ea0341445395caa87b
Lucas <lcasagrande@itba.edu.ar>
2017-12-08 09:24:04 -0300

    better pad collision

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -19,7 +19,16 @@ public class Ball : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.collider.tag == "Player") {
             Debug.Log("Hit Pad");
-            rb.velocity = new Vector2((transform.position.x - collision.collider.transform.position.x)* 2, rb.velocity.y);
+            float magnitude = rb.velocity.magnitude;
+            float dir = Mathf.Sign(rb.velocity.y);
+            float relativeDistanceToCenter = (transform.position.x - collision.collider.transform.position.x) / (collision.collider.bounds.size.x / 2);
+            rb.velocity = new Vector2(magnitude * Mathf.Sin(relativeDistanceToCenter * Mathf.PI / 3), dir*magnitude * Mathf.Cos(relativeDistanceToCenter * Mathf.PI / 3));
+            Debug.Log("Relative distance: " + relativeDistanceToCenter);
+            Debug.Log("Magnitude: " + magnitude);
+            //rb.velocity = new Vector2(* 2, rb.velocity.y);
+        }
+        if(collision.collider.tag == "Goals") {
+            Debug.Log("Game Over");
         }
     }
 }
