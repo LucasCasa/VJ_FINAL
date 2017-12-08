c77cccdd94b5d129686a842cdb9a326210250036
Lucas <lcasagrande@itba.edu.ar>
2017-12-08 18:51:54 -0300

    scripts folder, GmeManager script.

---
+++ Assets/Scripts/DevPreload.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevPreload : MonoBehaviour {
+
+    void Awake() {
+        GameObject check = GameObject.Find("GameManager");
+        if (check == null) { UnityEngine.SceneManagement.SceneManager.LoadScene("_preload"); }
+    }
+}
+
