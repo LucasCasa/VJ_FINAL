8953d3beea129272441a576ee9f4f11d42564dda
maggievega <maggievega@hotmail.com>
2017-12-18 22:38:33 -0300

    Menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+			
+	}
+
+    public void goToGame(){
+        SceneManager.LoadScene("Game");
+    }
+
+    public void exit(){
+        Application.Quit();
+    }
+}
